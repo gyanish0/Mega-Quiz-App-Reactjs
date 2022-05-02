faeddcebdd5b021b6495e79af1569353668a5a6e
gyanish77 <gyanish@luminoguru.com>
2022-05-02 17:19:32 +0530

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Made with ♥ by{" "}
+      <a href="https://github.com/gyanish0" style={{ cursor: "pointer" }}>
+        Gyanish Shrivastava
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
